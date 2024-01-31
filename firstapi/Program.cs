976e1221470206f1bde5c94fa8cafb13b3e9b61d
Priyankaa7 <126465709+Priyankaa7@users.noreply.github.com>
2024-01-31 11:26:42 +0530

    Add files via upload

---
+++ firstapi/Program.cs
@@ -0,0 +1,39 @@
+using firstapi.Models;
+using firstapi.Repository;
+using firstapi.Service;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<Ace52024Context>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddScoped<IFlight<PriyankaFlight>,FlightRepo>();
+builder.Services.AddScoped<IFlightServ<PriyankaFlight>, FlightServ>();
+builder.Services.AddScoped<ICustomer<PriyankaCustomer>,CustomerRepo>();
+builder.Services.AddScoped<ICustomerServ<PriyankaCustomer>, CustomerServ>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
