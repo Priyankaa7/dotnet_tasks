5f6de8ee62a18e006f3c6f524b5e35d5468c9beb
Priyankaa7 <126465709+Priyankaa7@users.noreply.github.com>
2024-01-31 11:22:19 +0530

    initial commit

---
+++ firstapi/Models/PriyankaCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace firstapi.Models;
+
+public partial class PriyankaCustomer
+{
+    public int CustomerId { get; set; }
+
+    public string? CustomerName { get; set; }
+
+    public string? Location { get; set; }
+
+    /*public virtual ICollection<PriyankaBooking> PriyankaBookings { get; set; } = new List<PriyankaBooking>();*/
+}
---
+++ firstapi/Models/PriyankaFlight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace firstapi.Models;
+
+public partial class PriyankaFlight
+{
+    public int FlightId { get; set; }
+
+    public string? FlightName { get; set; }
+
+    public string? Source { get; set; }
+
+    public string? Destination { get; set; }
+
+    public double? Rating { get; set; }
+
+    public double? CostPerTicket { get; set; }
+
+    /*public virtual ICollection<PriyankaBooking> PriyankaBookings { get; set; } = new List<PriyankaBooking>();*/
+}
